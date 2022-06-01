41c11ad26c27aeedd9c0a918f7fb2b2cc2d2076e
Ann Cascarano <4411121+redrambles@users.noreply.github.com>
2022-06-01 08:49:12 -0400

    Updates

---
+++ precache-manifest.133001eb6629d62391c5d943a25b7e0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "872b0ce074d62c34b498905711d67120",
+    "url": "/trivia-quiz/index.html"
+  },
+  {
+    "revision": "2e0f79e1953db8a45e89",
+    "url": "/trivia-quiz/static/css/main.629c4cc8.chunk.css"
+  },
+  {
+    "revision": "0589d4c132307a1af734",
+    "url": "/trivia-quiz/static/js/2.6833fa6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/trivia-quiz/static/js/2.6833fa6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e0f79e1953db8a45e89",
+    "url": "/trivia-quiz/static/js/main.78c85a85.chunk.js"
+  },
+  {
+    "revision": "8b35431d448f71242671",
+    "url": "/trivia-quiz/static/js/runtime-main.c36d0437.js"
+  }
+]);
